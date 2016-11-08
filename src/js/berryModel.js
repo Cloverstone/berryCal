5b7163f980e39e8d622afcfc9a37061be1546205
Adam Smallcomb <atsmallcomb@gmail.com>
2016-11-08 07:55:03 -0500

    Add basic functionality

--- src/js/berryModel.js
+++ src/js/berryModel.js
@@ -1,29 +1,20 @@
-
-function tableModel (owner, initial) {
+function berryModel (owner, initial, _id) {
 	this.owner = owner;
-	this.id = Berry.getUID();
+	this._id = _id || Berry.getUID();
 	this.attributes = {};
 	this.schema = owner.options.schema;
 	this.set = function(newAtts){
 		this.attributes = newAtts;
 	}
-	this.checked = false;
+	this.selected = false;
 	$.extend(true, this.attributes, initial);
 	this.toJSON = function() {return this.attributes}
 	this.delete = function(){
 		this.owner.models.splice(_.indexOf(_.pluck(this.owner.models, 'id'), this.id),1);
 	}
-	
 	this.events = {initialize: []};
 	this.addSub = Berry.prototype.addSub;
 	this.on = Berry.prototype.on;
 	this.off = Berry.prototype.off;
 	this.trigger = Berry.prototype.trigger;
-
 };
-
-// tableModel.prototype.events = {initialize: []};
-// tableModel.prototype.addSub = Berry.prototype.addSub;
-// tableModel.prototype.on = Berry.prototype.on;
-// tableModel.prototype.off = Berry.prototype.off;
-// tableModel.prototype.trigger = Berry.prototype.trigger;
